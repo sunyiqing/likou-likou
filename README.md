9ba873a4b76f0c702bc99c8dff5d27ce621262c8
suneqing@163.com <suneqing@163.com>
2020-07-18 19:59:26 +0800

    整数反转

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
  \#   | Title         | Solution   |
 ------|---------------|------------|
- 1  | [两数之和](https://leetcode-cn.com/problems/two-sum/) | [java](src/main/java/li/kou/TheSumOfTwoNums.java) |
- 2  | [整数反转](https://leetcode-cn.com/problems/reverse-integer/) | [java](src/main/java/li/kou/ReverseInteger.java) |
+ 1  | [两数之和](https://leetcode-cn.com/problems/two-sum/) | [java](https://github.com/sunyiqing/likou-likou/blob/master/src/main/java/com/li/kou/TheSumOfTwoNums.java) |
+ 2  | [整数反转](https://leetcode-cn.com/problems/reverse-integer/) | [java](https://github.com/sunyiqing/likou-likou/blob/master/src/main/java/com/li/kou/ReverseInteger.java) |
--- src/main/java/com/li/kou/ReverseInteger.java
+++ src/main/java/com/li/kou/ReverseInteger.java
@@ -8,9 +8,44 @@ public class ReverseInteger {
 
     public static void main(String[] args) {
 
+        int s1 = soulution(123);
+        System.out.print(s1);
+        int s2 = soulution(-123);
+        System.out.print(s2);
+        int s3 = soulution(-120);
+        System.out.print(s3);
+        int s4 = soulution1(123);
+        System.out.print(s4);
+        int s5 = soulution1(-120);
+        System.out.print(s5);
 
     }
 
+    private static int soulution(int num){
+        long temp = 0;
+        while (num != 0){
+            temp = temp * 10 + num % 10;
+            if (temp > Integer.MAX_VALUE || temp < Integer.MIN_VALUE){
+                return 0;
+            }
+            num = num / 10;
+        }
+        return (int)temp;
+    }
+
+
+    protected static int soulution1(int num){
+        int ans = 0;
+        while (num != 0){
+            if((ans * 10) / 10 != ans){
+                ans = 0;
+                break;
+            }
+            ans = ans * 10 + num % 10;
+            num = num / 10;
+        }
+        return ans;
+    }
 
 
 }
