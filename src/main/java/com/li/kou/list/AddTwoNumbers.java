29c7aaf5e428d09a8f11a7476610e24b46160437
sunyiqing <sunyiqing@imdada.cn>
2020-08-12 18:16:18 +0800

    RemoveNthNodeFromEndOfList

--- src/main/java/com/li/kou/list/AddTwoNumbers.java
+++ src/main/java/com/li/kou/list/AddTwoNumbers.java
@@ -1,4 +1,4 @@
-package com.li.kou;
+package com.li.kou.list;
 
 /**
  *
---
+++ src/main/java/com/li/kou/list/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,63 @@
+package com.li.kou.list;
+
+/**
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ *
+ * 示例：
+ *
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ *
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ * 说明：
+ *
+ * 给定的 n 保证是有效的。
+ *
+ * 进阶：
+ *
+ * 你能尝试使用一趟扫描实现吗？
+ *
+ */
+public class RemoveNthNodeFromEndOfList {
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l11 = new ListNode(2);
+        ListNode l12 = new ListNode(4);
+        ListNode l13 = new ListNode(5);
+        ListNode l14 = new ListNode(6);
+        l1.next = l11;
+        l11.next = l12;
+        l12.next = l13;
+        l13.next = l14;
+        RemoveNthNodeFromEndOfList removeNthNodeFromEndOfList = new RemoveNthNodeFromEndOfList();
+
+        removeNthNodeFromEndOfList.removeNthFromEnd1(l1,1);
+
+    }
+
+    public ListNode removeNthFromEnd1(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        // Advances first pointer so that the gap between first and second is n nodes apart
+        for (int i = 1; i <= n + 1; i++) {
+            first = first.next;
+        }
+        // Move first to the end, maintaining the gap
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+
+
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+}
