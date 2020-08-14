ebd9cd25a6be2901b94f6d7e0e43a1c388f21fcf
sunyiqing <sunyiqing@imdada.cn>
2020-08-14 12:44:41 +0800

    ReverseLinkedListii

--- src/main/java/com/li/kou/list/ReverseLinkedListii.java
+++ src/main/java/com/li/kou/list/ReverseLinkedListii.java
@@ -1,5 +1,7 @@
 package com.li.kou.list;
 
+import com.li.kou.common.ListNode;
+
 /**
  * 反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
 
@@ -15,4 +17,20 @@ package com.li.kou.list;
  * Created by yiqing on 2020/8/14.
  */
 public class ReverseLinkedListii {
+
+    public static void main(String[] args) {
+        ListNode l1 = new  ListNode(1);
+        ListNode l11 = new ListNode(2);
+        ListNode l12 = new ListNode(4);
+        ListNode l13 = new ListNode(5);
+        ListNode l14 = new ListNode(6);
+        l1.next = l11;
+        l11.next = l12;
+        l12.next = l13;
+        l13.next = l14;
+
+    }
+
+
+
 }
