42d3c8301d7e267f4f888757b0ebfca8035d1992
sunyiqing <sunyiqing@imdada.cn>
2020-08-17 13:45:00 +0800

    RemoveLinkedListElements

--- src/main/java/com/li/kou/list/RemoveLinkedListElements.java
+++ src/main/java/com/li/kou/list/RemoveLinkedListElements.java
@@ -16,9 +16,9 @@ public class RemoveLinkedListElements {
     public static void main(String[] args) {
         ListNode l1 = new ListNode(1);
         ListNode l11 = new ListNode(2);
-        ListNode l12 = new ListNode(4);
-        ListNode l13 = new ListNode(5);
-        ListNode l14 = new ListNode(6);
+        ListNode l12 = new ListNode(1);
+        ListNode l13 = new ListNode(1);
+        ListNode l14 = new ListNode(1);
         l1.next = l11;
         l11.next = l12;
         l12.next = l13;
@@ -30,18 +30,24 @@ public class RemoveLinkedListElements {
     public static ListNode removeElements(ListNode head, int val) {
         ListNode dummy = new ListNode(-1);
         dummy.next = head;
+
+        //用于删除节点
         ListNode prev = dummy;
+        //用于节点移动
         ListNode curr = head;
+
 
         while (curr != null){
             if (curr.val == val){
+                //相等 直接指向下一个
                 prev.next = curr.next;
             }else {
+                //prev 与 curr 一起移动
                 prev = curr;
             }
+            //curr 移动
             curr = curr.next;
         }
-
         return dummy.next;
     }
 
