7f2cf4ac9e643338d340b93a5388005d45bd93ff
sunyiqing <sunyiqing@imdada.cn>
2020-09-07 20:39:17 +0800

    LargestRectangleInHistogram
    没有看明白

--- src/main/java/com/li/kou/stack/LargestRectangleInHistogram.java
+++ src/main/java/com/li/kou/stack/LargestRectangleInHistogram.java
@@ -1,5 +1,7 @@
 package com.li.kou.stack;
 
+import java.util.Stack;
+
 /**
  * 给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
  *
@@ -31,10 +33,12 @@ package com.li.kou.stack;
 public class LargestRectangleInHistogram {
 
     public static void main(String[] args) {
-        int[] heights = {2,1,5,6,2,3};
-
+//        int[] heights = {2,1,5,6,2,3};
+//        int[] heights = {0,9};
+        int[] heights = {2,1,5};
         LargestRectangleInHistogram l = new LargestRectangleInHistogram();
         System.out.println(l.largestRectangleArea1(heights));
+        System.out.println(l.largestRectangleArea(heights));
     }
 
 
@@ -70,4 +74,27 @@ public class LargestRectangleInHistogram {
 
         return result;
     }
+
+
+    public int largestRectangleArea(int[] heights) {
+        // 这里为了代码简便，在柱体数组的头和尾加了两个高度为 0 的柱体。
+        int[] tmp = new int[heights.length + 2];
+        System.arraycopy(heights, 0, tmp, 1, heights.length);
+
+        Stack<Integer> stack = new Stack<Integer>();
+        int area = 0;
+        for (int i = 0; i < tmp.length; i++) {
+            // 对栈中柱体来说，栈中的下一个柱体就是其「左边第一个小于自身的柱体」；
+            // 若当前柱体 i 的高度小于栈顶柱体的高度，说明 i 是栈顶柱体的「右边第一个小于栈顶柱体的柱体」。
+            // 因此以栈顶柱体为高的矩形的左右宽度边界就确定了，可以计算面积 ～
+            while (!stack.isEmpty() && tmp[i] < tmp[stack.peek()]) {
+                int h = tmp[stack.pop()];
+                area = Math.max(area, (i - stack.peek() - 1) * h);
+            }
+            stack.push(i);
+        }
+
+        return area;
+    }
+
 }
